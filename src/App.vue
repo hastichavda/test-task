c68c488c2d25fcfcdab4ae6374cbf69b6d2c32cb
hastichavda <hasiichavda10@gmail.com>
2021-11-23 00:34:57 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <button @click.prevent="submit">Submit</button>
+    <BaseAxios
+      v-if="isAxios"
+      methods="get"
+      url="https://jsonplaceholder.typicode.com/todos/1"
+      title="All Todos"
+      name="todos"
+    />
+  </div>
+</template>
+
+<script setup>
+import BaseAxios from './components/BaseAxios.vue'
+import { ref } from 'vue'
+const isAxios = ref(false)
+
+function submit() {
+  isAxios.value = true
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BaseAxios.vue
@@ -0,0 +1,41 @@
+<template>
+  <div style="margin-top: 10px">{{ title }}</div>
+  <div style="margin-top: 10px">{{ name }}</div>
+</template>
+<script setup>
+import axios from 'axios'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: '',
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  data: {
+    type: [String, Number, Array],
+    default: '',
+  },
+  url: {
+    type: String,
+    default: '',
+  },
+  methods: {
+    type: String,
+    default: '',
+  },
+})
+
+loadAction()
+
+async function loadAction() {
+  let res = await axios({
+    method: props.methods,
+    url: props.url,
+    data: props.data,
+  })
+  console.log('Response =>', res)
+}
+</script>
